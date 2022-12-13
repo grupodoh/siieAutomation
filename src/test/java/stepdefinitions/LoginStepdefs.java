16fbd295770956b0f785420f60da0a1682871188
jaun diego moncaleano quiñones <grupoempresarialdohcolombia@gmail.com>
2022-12-13 10:50:18 -0500

    continuaciión de automatizacion

--- src/test/java/stepdefinitions/LoginStepdefs.java
+++ src/test/java/stepdefinitions/LoginStepdefs.java
@@ -3,7 +3,7 @@ package stepdefinitions;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
+//import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
